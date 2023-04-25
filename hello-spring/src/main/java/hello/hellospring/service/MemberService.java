2f0d0170fc842fd2230311c3f78987241030c26b
jihyang <jihyangi043@gmail.com>
2023-04-25 13:50:23 +0900

    feat : add anotation

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     // 회원 서비스를 만들기 위해 MemberRepository 인스턴스 필요
     private final MemberRepository memberRepository;
 
+    // Autowired 생략(생성자가 한 개만 있기 때문)
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
